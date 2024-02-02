91f65c47d71ad34f475029980b44d1e7e30257d7
Ziyang Wen <wenziyang007@gmail.com>
2024-02-02 19:12:36 +0800

    Set up Entities

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f301841ac8b4a500f0fa0cbbc58c228187a07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc53a9410b160b9f27da1ec43b3b8b52119ece7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
