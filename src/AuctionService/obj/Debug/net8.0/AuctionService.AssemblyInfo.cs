b3be792965cbbf1932843bbb5ac429818a7703a6
Ziyang Wen <wenziyang007@gmail.com>
2024-02-04 22:58:58 +0800

    Add gitignore file

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4dab241b5ac83e5a07785822436f044c70b2d90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef8cd52fc6d995562d1ad3d360445e3b220a2d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
