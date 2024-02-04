d84c3fc0ae877463f731d9f26f01f83e2218b5c2
Ziyang Wen <wenziyang007@gmail.com>
2024-02-04 16:00:59 +0800

    create seed data and shape dtos

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20474b76997db210b127fc5333b1ae046d47a2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70510432b8906f5df0fed018bd50bd9fd124634a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
