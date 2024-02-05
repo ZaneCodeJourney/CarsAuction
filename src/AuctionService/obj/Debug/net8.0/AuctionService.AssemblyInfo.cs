b9fa5d331ffd9ae4538d0cb0ed96c43bfbb2bcec
Ziyang Wen <wenziyang007@gmail.com>
2024-02-05 20:51:45 +0800

    Create the Serching Microservice

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef8cd52fc6d995562d1ad3d360445e3b220a2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3be792965cbbf1932843bbb5ac429818a7703a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
