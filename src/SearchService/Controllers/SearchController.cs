91db262cb4b233ba4424a1c906269418ef8b0978
Ziyang Wen <wenziyang007@gmail.com>
2024-02-06 10:30:28 +0800

    Search Controller

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string SearchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(SearchTerm))
+        {
+            query.Match(Search.Full, SearchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
