e9d7ebe3243b39941fcf0cc1dc5bd89caae06ca7
Ziyang Wen <wenziyang007@gmail.com>
2024-02-06 10:11:59 +0800

    Create model and add seed data

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3be792965cbbf1932843bbb5ac429818a7703a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f9159203971ff33d0839bdfb59daf240b9e2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
